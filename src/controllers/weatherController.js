f2e05282b7094ecd64c331fd2ec8e94161f2debb
mrinalsinha2211 <145749474+mrinalsinha2211@users.noreply.github.com>
2024-01-07 20:02:03 +0530

    Update weatherController.js

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -27,13 +27,35 @@ async function saveDataToDatabase(data) {
 
 // Level 1: Get City Weather Data by Name
 async function getWeatherDataByName(cityName) {
-   const data=await getDataFromDatabase();
-  const cityData=data.find((cityData)=>cityData.city.toLowercase()===cityName.toLowercase());
-  if(cityData){
-    return cityData.weather;
-  }else{
-    throw new Error("Not found");
+  //  const data=await getDataFromDatabase();
+  // const cityData=data.find((cityData)=>cityData.city.toLowercase()===cityName.toLowercase());
+  // if(cityData){
+  //   return cityData.weather;
+  // }else{
+  //   throw new Error("Not found");
+  // }
+  const data = await getDataFromDatabase();
+  const cityData = data.find((cityData) => cityData.city.toLowerCase() === cityName.toLowerCase());
+  if (cityData) {
+    return {
+      status: 'success',
+      message: 'Weather data retrieved',
+      data: {
+        city: cityData.city,
+        temperature: cityData.weather.temperature,
+        humidity: cityData.weather.humidity,
+        windSpeed: cityData.weather.windSpeed,
+        conditions: cityData.weather.conditions,
+      },
+    };
+  } else {
+    throw {
+      status: 'error',
+      message: 'Failed to retrieve weather data',
+      error: 'City not found',
+    };
   }
+  
 }
 
 
